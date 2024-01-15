606d4a2d88d50bd63f605e376e42b68b4cd2b70f
Olivera2708 <olivera.radovanovic2002@gmail.com>
2024-01-15 01:44:42 +0100

    added test for service

--- Bookify/src/app/feature-modules/accommodation/accommodation-page/accommodation-page.component.spec.ts
+++ Bookify/src/app/feature-modules/accommodation/accommodation-page/accommodation-page.component.spec.ts
@@ -1,17 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { AccommodationPageComponent } from './accommodation-page.component';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AccountService} from "../../account/account.service";
+import {AccommodationService} from "../accommodation.service";
+import {ActivatedRoute, RouterModule} from "@angular/router";
+import {AuthenticationService} from "../../authentication/authentication.service";
+import {MatDialog} from "@angular/material/dialog";
+import {of} from "rxjs";
+import {CarouselComponent} from "../carousel/carousel.component";
+import {MapComponent} from "../../../shared/map/map.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ReserveComponent} from "../reserve/reserve.component";
+import {DatapickerRangeComponent} from "../../../layout/datapicker-range/datapicker-range.component";
+import {FormsModule} from "@angular/forms";
+import {MessageDialogComponent} from "../../../layout/message-dialog/message-dialog.component";
+import {ReservationDialogComponent} from "../../../layout/reservation-dialog/reservation-dialog.component";
+import {By} from "@angular/platform-browser";
 
 describe('AccommodationPageComponent', () => {
   let component: AccommodationPageComponent;
   let fixture: ComponentFixture<AccommodationPageComponent>;
+  let matDialogOpenSpy: jasmine.Spy;
+  let accommodationServiceSpy: jasmine.SpyObj<AccommodationService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AccommodationPageComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    matDialogOpenSpy = jasmine.createSpy('open');
+    accommodationServiceSpy = jasmine.createSpyObj('AccommodationService', ['getTotalPrice', 'createReservationRequest']);
+
+    TestBed.configureTestingModule({
+      declarations: [AccommodationPageComponent, MapComponent, ReserveComponent],
+      imports: [CarouselComponent, HttpClientModule, DatapickerRangeComponent, RouterModule, FormsModule],
+      providers: [
+        { provide: MatDialog, useValue: { open: matDialogOpenSpy } },
+        { provide: AccommodationService, useValue: accommodationServiceSpy },
+        { provide: AuthenticationService, useValue: { getRole: () => 'GUEST' } },
+        { provide: ActivatedRoute, useValue: { params: of({ accommodationId: 1 }) } },
+        { provide: AccountService, useValue: {} },
+        { provide: MatSnackBar, useValue: {} },
+      ],
+    });
+
     fixture = TestBed.createComponent(AccommodationPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +49,97 @@ describe('AccommodationPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should open message dialog if accommodation is not available", fakeAsync(() => {
+    accommodationServiceSpy.getTotalPrice.and.returnValue(of(-1));
+    component.openDialog(1, new Date(124, 2, 1), new Date(124, 2, 5), 2, "ROOM");
+
+    tick();
+
+    expect(matDialogOpenSpy).toHaveBeenCalledWith(
+      MessageDialogComponent,
+      jasmine.objectContaining({
+        data: { message: "Accommodation it not available for this parameters." },
+      })
+    );
+
+    expect(accommodationServiceSpy.createReservationRequest).not.toHaveBeenCalled();
+  }));
+
+  // it('should open ReservationDialog if accommodation is available', fakeAsync(() => {
+  //   // Arrange
+  //   const fakeTotalPrice = 80.0;
+  //   accommodationServiceSpy.getTotalPrice.and.returnValue(of(fakeTotalPrice));
+  //
+  //   // Act
+  //   component.openDialog(1, new Date(124, 3, 6), new Date(124, 3, 8), 2, "ROOM");
+  //   tick();
+  //
+  //   // Assert
+  //   expect(accommodationServiceSpy.getTotalPrice).toHaveBeenCalledWith(1, jasmine.any(Date), jasmine.any(Date), 'ROOM', 2);
+  //
+  //   // Wait for the asynchronous behavior to complete
+  //   fixture.detectChanges();
+  //   tick();
+  //
+  //   // Assert the dialog opening
+  //   expect(matDialogOpenSpy).toHaveBeenCalledWith(ReservationDialogComponent, jasmine.objectContaining({
+  //     data: { message: `Total cost for this reservation is ${fakeTotalPrice} EUR.` },
+  //   }));
+  //
+  //   expect(accommodationServiceSpy.createReservationRequest).not.toHaveBeenCalled();
+  // }));
+
+
+  // it('should open ReservationDialog if accommodation is available', fakeAsync(() => {
+  //   accommodationServiceSpy.getTotalPrice.and.returnValue(of(80.0));
+  //   matDialogOpenSpy.and.returnValue({
+  //     afterClosed: () => of(true),
+  //   });
+  //
+  //   component.openDialog(1, new Date(124, 3, 1), new Date(124, 3, 5), 2, "ROOM");
+  //   tick();
+  //
+  //
+  //   expect(matDialogOpenSpy).toHaveBeenCalledWith(ReservationDialogComponent, jasmine.objectContaining({
+  //     data: { message: `Total cost for this reservation is 80.0 EUR.` },
+  //   }));
+  //   // expect(accommodationServiceSpy.createReservationRequest).not.toHaveBeenCalled();
+  //
+  //   // matDialogOpenSpy.calls.mostRecent().returnValue.afterClosed().next(true);
+  //   // tick();
+  //   //
+  //   // // Assert additional actions after user confirms reservation
+  //   // expect(accommodationServiceSpy.createReservationRequest).toHaveBeenCalledOnceWith(
+  //   //   jasmine.objectContaining({
+  //   //     guestNumber: 2,
+  //   //     price: Math.round(fakeTotalPrice * 100) / 100,
+  //   //   }),
+  //   //   1,
+  //   //   jasmine.any(String)
+  //   // );
+  // }));
+
+  // it("should open reservation dialog if accommodation is available", fakeAsync(() => {
+  //   accommodationServiceSpy.getTotalPrice.and.returnValue(of(80.0));
+  //   component.openDialog(1, new Date(2024, 3, 6), new Date(2024, 3, 8), 2, "ROOM");
+  //
+  //   tick();
+  //
+  //   expect(matDialogOpenSpy).toHaveBeenCalledWith(
+  //     ReservationDialogComponent,
+  //     jasmine.objectContaining({
+  //       data: { message: "Total cost for this reservation is 80.0 EUR." },
+  //     })
+  //   );
+  //
+  //   // expect(accommodationServiceSpy.createReservationRequest).toHaveBeenCalledOnceWith(
+  //   //   jasmine.objectContaining({
+  //   //     guestNumber: 2,
+  //   //     price: 80.0,
+  //   //   }),
+  //   //   1,
+  //   //   jasmine.any(String)
+  //   // );
+  // }));
 });
--- Bookify/src/app/feature-modules/accommodation/accommodation.service.spec.ts
+++ Bookify/src/app/feature-modules/accommodation/accommodation.service.spec.ts
@@ -1,16 +1,107 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AccommodationService } from './accommodation.service';
+import moment from "moment";
+import {ReservationRequestDTO} from "./model/reservation-request.dto.model";
+import {Reservation} from "./model/reservation.model";
+import {GuestDTO} from "../account/model/guest.dto";
+import {Accommodation} from "./model/accommodation.model";
+import {Address} from "../account/model/address";
 
 describe('AccommodationService', () => {
   let service: AccommodationService;
+  let httpController: HttpTestingController;
+  let url = 'http://localhost:8080/api/v1/';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AccommodationService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the total price', () => {
+    const accommodationId = 1;
+    const begin = new Date('2024-03-06');
+    const end = new Date('2024-03-08');
+    const pricePer = 'ROOM';
+    const persons = 2;
+    const expectedTotalPrice = 100;
+
+    service.getTotalPrice(accommodationId, begin, end, pricePer, persons).subscribe((totalPrice) => {
+      expect(totalPrice).toBe(expectedTotalPrice);
+    });
+
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${url}accommodations/price?id=${accommodationId}&begin=${moment(begin).format('DD.MM.YYYY')}&end=${moment(end).format('DD.MM.YYYY')}&pricePer=${pricePer}&persons=${persons}`
+    })
+
+    req.flush(expectedTotalPrice);
+  });
+
+  it('should create a reservation request', () => {
+    const reservation: ReservationRequestDTO = {
+      created: new Date(),
+      start: new Date('2024-03-01'),
+      end: new Date('2024-03-04'),
+      guestNumber: 2,
+      price: 100
+    };
+
+    const accommodationId = 1;
+    const guestId = 2;
+
+    const expectedReservation: Reservation = {
+      created: reservation.created,
+      start: reservation.start,
+      end: reservation.end,
+      guestNumber: reservation.guestNumber,
+      guest: {
+        id: guestId,
+        firstName: 'John',
+        lastName: 'Doe',
+      } as GuestDTO,
+      accommodation: {
+        id: accommodationId,
+        name: 'Example Accommodation',
+        description: 'Accommodation description',
+        minGuest: 2,
+        maxGuest: 4,
+        cancellationDeadline: 2,
+        manual: false,
+        filters: ['BAR'],
+        accommodationType: 'HOTEL',
+        pricePer: 'ROOM',
+        address: {
+          country: 'Example Country',
+          city: 'Example City',
+          address: 'Example Street 123',
+          zipCode: '12345',
+        } as Address,
+      } as Accommodation,
+      status: 'PENDING',
+    };
+
+    service.createReservationRequest(reservation, accommodationId, guestId).subscribe((createdReservation) => {
+      expect(createdReservation).toEqual(expectedReservation);
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: `${url}reservations/create?accommodationId=${accommodationId}&guestId=${guestId}`
+    });
+
+    req.flush(expectedReservation);
   });
 });
