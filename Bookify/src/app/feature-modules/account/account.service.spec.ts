c6dc7a327575544484322f1fbd70b73bf5d45d43
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2024-01-21 04:40:48 +0100

    add tests for account.service

--- Bookify/src/app/feature-modules/account/account.service.spec.ts
+++ Bookify/src/app/feature-modules/account/account.service.spec.ts
@@ -1,7 +1,9 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AccountService } from './account.service';
+import {AccountService} from './account.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {account1, account3} from "./mocks/accounts.mock";
+import {environment} from "../../../env/env";
 
 describe('AccountService', () => {
   let service: AccountService;
@@ -9,7 +11,7 @@ describe('AccountService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule]
+      imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(AccountService);
     httpController = TestBed.inject(HttpTestingController);
@@ -23,6 +25,28 @@ describe('AccountService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should call getUser and return account1 from account.mocks.ts', () => {
+    const userId = 1;
+    service.getUser(userId).subscribe((res) => {
+      expect(res).toEqual(account1);
+    });
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: environment.apiHost + "users/" + userId
+    });
+    req.flush(account1);
+  });
+
+  it('should call updateUser and return updated account (account2 from account.mocks.ts)', ()=>{
+    service.updateUser(account3).subscribe((res) => {
+      expect(res).toEqual(account3);
+    });
+    const req = httpController.expectOne({
+      method: 'PUT',
+      url: environment.apiHost + "users",
+    });
+    req.flush(account3);
+  });
 
 
 });
